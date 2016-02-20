9e176dfef3e6394c825a78ea9aa9f886200a6f44
Stijn Raeymaekers <raeymaekers.stijn@gmail.com>
2016-02-20 11:54:17 +0100

    Updater
    Copied & modified from Maze Creator.

---
+++ TrinityCreator/Updater.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Windows;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace TrinityCreator
+{
+    class Updater
+    {
+        public static void CheckLatestVersion()
+        {
+            string local = GetLocalVersionNumber();
+            string latest = String.Empty;
+            try
+            {
+                latest = GetLatestVersionNumber(); 
+            }
+            catch
+            {
+                MessageBox.Show("Could not check the latest version online", "Can't check version", MessageBoxButton.OK, MessageBoxImage.Information);
+                latest = local;
+            }
+
+            if (local != latest)
+            {
+                MessageBoxResult result = MessageBox.Show("A newer version of Trinity Creator is available. Would you like to quick-install it?", "Update available",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                    StartUpdate();
+            }
+        }
+
+        private static void StartUpdate()
+        {
+            try
+            {
+                string updateFile = Path.GetTempFileName() + ".exe";
+                WebClient c = new WebClient();
+                c.DownloadFile("https://github.com/RStijn/TrinityCreator/blob/master/Updater/bin/Release/TrinityUpdater.exe?raw=true", updateFile);
+                string currentExe = Assembly.GetExecutingAssembly().Location;
+
+                ProcessStartInfo proc = new ProcessStartInfo(updateFile);
+                proc.Arguments = "\"" + currentExe + "\"";
+                Process.Start(proc);
+                Environment.Exit(0);
+            }
+            catch
+            {
+                MessageBox.Show("Please update manually", "Update failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        /// <summary>
+        /// Reads AssemblyFileVersion from GitHub
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLatestVersionNumber()
+        {
+            string version = String.Empty;
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("https://raw.githubusercontent.com/RStijn/TrinityCreator/master/TrinityCreator/Properties/AssemblyInfo.cs");
+            StreamReader reader = new StreamReader(stream);
+            string[] lines = Regex.Split(reader.ReadToEnd(), "\r\n|\r|\n");
+            foreach (string line in lines)
+            {
+                if (line.Contains("AssemblyFileVersion"))
+                    return line.Replace("[assembly: AssemblyFileVersion(\"", String.Empty).Replace("\")]", String.Empty);
+            }
+            throw new Exception();
+        }
+
+        public static string GetLocalVersionNumber()
+        {
+            Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            return fvi.FileVersion;
+        }
+    }
+}
---
+++ Updater/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TrinityCreatorUpdater
+{
+    class Program
+    {
+        static string path = String.Empty;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("- Trinity Creator Updater -");
+            path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\TrinityCreator.exe";
+            if (args.Length > 0)
+                path = args[0];
+
+            while (Process.GetProcessesByName("TrinityCreator").Length != 0)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Waiting for Trinity Creator to close...");
+            }
+            
+            StartUpdate();
+            Thread.Sleep(1000);
+            Environment.Exit(0);
+        }
+
+        private static void StartUpdate()
+        {
+            Console.WriteLine("Downloading latest version of Trinity Creator...");
+            WebClient c = new WebClient();
+            c.DownloadFile("https://github.com/RStijn/TrinityCreator/blob/master/TrinityCreator/bin/Release/TrinityCreator.exe?raw=true", path);
+
+            Console.WriteLine("Downloaded to {0}", path);
+            Console.WriteLine("Opening Trinity Creator...");
+            Process.Start(path);
+        }
+    }
+}
