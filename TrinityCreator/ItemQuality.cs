4a1426fd813bf047d374125efd4b751e4b6ff948
RStijn <raeymaekers.stijn@gmail.com>
2016-03-05 17:20:35 +0100

    Accurate quality colors

--- TrinityCreator/ItemQuality.cs
+++ TrinityCreator/ItemQuality.cs
@@ -78,14 +78,14 @@ namespace TrinityCreator
         public static ItemQuality[] GetQualityList()
         {
             return new ItemQuality[] {
-                new ItemQuality(0, "Poor", Colors.Gray),
-                new ItemQuality(1, "Common", Colors.White),
-                new ItemQuality(2, "Uncommon", Colors.Green),
-                new ItemQuality(3, "Rare", Colors.Blue),
-                new ItemQuality(4, "Epic", Colors.Purple),
-                new ItemQuality(5, "Legendary", Colors.Orange),
+                new ItemQuality(0, "Poor", Color.FromRgb(157, 157, 157)),
+                new ItemQuality(1, "Common", Color.FromRgb(255, 255, 255)),
+                new ItemQuality(2, "Uncommon", Color.FromRgb(30, 255, 0)),
+                new ItemQuality(3, "Rare", Color.FromRgb(0, 112, 221)),
+                new ItemQuality(4, "Epic", Color.FromRgb(163, 53, 238)),
+                new ItemQuality(5, "Legendary", Color.FromRgb(255, 128, 0)),
                 new ItemQuality(6, "Artifact", Colors.Red),
-                new ItemQuality(7, "Bind to Account", Colors.Gold),
+                new ItemQuality(7, "Bind to Account", Color.FromRgb(229, 204, 128)),
             };
         }
     }
