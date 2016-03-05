932b47fa11c6901871e01796e6b2c8af26da9f1f
RStijn <raeymaekers.stijn@gmail.com>
2016-03-05 23:18:04 +0100

    App culture set to en-US
    Avoids issues with double period turning into comma.

--- TrinityCreator/App.xaml.cs
+++ TrinityCreator/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
+using System.Globalization;
+using System.Windows.Markup;
 
 namespace TrinityCreator
 {
@@ -13,5 +16,12 @@ namespace TrinityCreator
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            FrameworkElement.LanguageProperty.OverrideMetadata(typeof(FrameworkElement), new FrameworkPropertyMetadata(
+                        XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+        }
     }
 }
