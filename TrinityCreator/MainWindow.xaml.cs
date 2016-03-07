6208a2a73c10d573f1631a25100fad40b648d837
RStijn <raeymaekers.stijn@gmail.com>
2016-03-07 23:08:52 +0100

    Configuration & motivation buttons added :+1:

--- TrinityCreator/MainWindow.xaml.cs
+++ TrinityCreator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace TrinityCreator
 {
@@ -32,6 +33,27 @@ namespace TrinityCreator
             Item item = new Item();
             ItemPreview preview = new ItemPreview(item);
             armorWeaponsTab.Content = new ItemPage(item, preview);
+        }
+
+        private void Donate_Click(object sender, RoutedEventArgs e)
+        {
+            Donate();
+        }
+
+        private void WhyDonate_Click(object sender, RoutedEventArgs e)
+        {
+            string msg = string.Format("Trinity Creator is released as an open source project created by a passionate IT student, so donating is completely optional.{0}{0}"+
+                "If you're new to the emulation scene, you can use this program to create your own world and even release it as a public server.{0}" +
+                "Or if you're already running a profitable server, you'll be able to save a lot of development time when releasing new content.{0}{0}"+
+                "So, do you want to motivate me to implement more features or thank me for making things easier? Then toss me a few bucks! :)", Environment.NewLine);
+            var result = MessageBox.Show(msg, "Why donate?", MessageBoxButton.OKCancel);
+            if (result == MessageBoxResult.OK)
+                Donate();
+        }
+
+        private void Donate()
+        {
+            Process.Start("https://paypal.me/RStijn");
         }
     }
 }
