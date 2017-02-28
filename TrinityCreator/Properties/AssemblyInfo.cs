91a685d654e96dcb74c370844f8e1ab58374b5e4
Nadromar <nadromar@gmail.com>
2017-02-28 04:36:54 +0100

    v1.0.6.21

--- TrinityCreator/Properties/AssemblyInfo.cs
+++ TrinityCreator/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.6.20")]
-[assembly: AssemblyFileVersion("1.0.6.20")]
+[assembly: AssemblyVersion("1.0.6.21")]
+[assembly: AssemblyFileVersion("1.0.6.21")]
