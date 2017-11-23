c69515b12d7fb1001534e886a3a4587e0d8c22c7
Nadromar </>
2017-11-23 12:23:27 +0100

    Windows 7 version (clarification)
    This could be important later.

--- TrinityCreator/Properties/AssemblyInfo.cs
+++ TrinityCreator/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("TrinityCreator")]
+[assembly: AssemblyTitle("TrinityCreatorWin7")]
 [assembly: AssemblyDescription("Easily create content for a 3.3.5a TrinityCore emulator server.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nadromar")]
