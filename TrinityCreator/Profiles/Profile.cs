4e07c2c19acc0799008abfb98056c07dd6c83f55
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2019-12-31 01:23:17 +0100

    Profile Structure

--- TrinityCreator/Profiles/Profile.cs
+++ TrinityCreator/Profiles/Profile.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,23 +7,45 @@ using System.Threading.Tasks;
 
 namespace TrinityCreator.Profiles
 {
+    // Structure: {"Name":"Test Profile","EmulatorName":null,"GameVersion":null,"DatabaseVersion":null,"Revision":1,"Author":"NotCoffee418","UpdateUrl":"https://exaple.com/MyProfile.json","Bindings":{"appKey":"exportKey","appKey2":"exportKey2"}}
     class Profile
     {
+        #region Static 
         static Profile _activeProfile = null;
 
-        public static Profile ActiveProfile {
+        public static Profile Active {
             get { return _activeProfile; }
             private set { _activeProfile = value; }
         }
 
-        public static void LoadString(string profileJson)
+        public static Profile LoadFile(string filePath)
         {
-
+            throw new NotImplementedException();
         }
 
-        public static void LoadFile(string filePath)
+        public static List<Profile> ListProfiles()
         {
-
+            throw new NotImplementedException();
         }
+        #endregion
+
+        public string Name { get; set; }
+
+        public string EmulatorName { get; set; }
+
+        public string GameVersion { get; set; }
+
+        public string DatabaseVersion { get; set; }
+
+        public int Revision { get; set; }
+
+        public string Author { get; set; }
+
+        public string UpdateUrl { get; set; }
+
+        [JsonIgnore] 
+        public string LocalPath { get; set; }
+
+        public Dictionary<string, string> Bindings { get; set; }
     }
 }
