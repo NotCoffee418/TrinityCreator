c7ea8259b7f9eb2e67c4ea5e2f8fad7925a4da14
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-01-30 21:50:16 +0100

    v1.1.0.110 (required update)
    Not updating will cause the latest TC335a profile to break exports.
    - Support for quest starter/ender type (choose between creature & gameobject)
    - Check for updates menu option

--- TrinityCreator/Properties/AssemblyInfo.cs
+++ TrinityCreator/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.109")]
-[assembly: AssemblyFileVersion("1.1.0.109")]
+[assembly: AssemblyVersion("1.1.0.110")]
+[assembly: AssemblyFileVersion("1.1.0.110")]
