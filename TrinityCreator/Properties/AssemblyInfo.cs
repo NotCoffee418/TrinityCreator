0c27cce0293a76a1b50460b397b6b2119a5c1906
NotCoffee418 <9306304+NotCoffee418@users.noreply.github.com>
2020-01-30 23:55:43 +0100

    v1.1.0.111
    - Lookup tool profile support for gameobject and spell lookup
    - Profiles have a TestedBuild field. Automatic profile updates are disabled if TCreator build is older than TestedBuild.

--- TrinityCreator/Properties/AssemblyInfo.cs
+++ TrinityCreator/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.0.110")]
-[assembly: AssemblyFileVersion("1.1.0.110")]
+[assembly: AssemblyVersion("1.1.0.111")]
+[assembly: AssemblyFileVersion("1.1.0.111")]
